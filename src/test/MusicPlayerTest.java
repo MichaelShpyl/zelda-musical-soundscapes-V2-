a493b5800f6ac12b951fc6d6ef6b8fed2f30e143
MichaelShpyl <150159875+MichaelShpyl@users.noreply.github.com>
2024-12-22 18:01:38 +0000

    Update MusicPlayerTest.java

--- src/test/MusicPlayerTest.java
+++ src/test/MusicPlayerTest.java
@@ -1,13 +1,24 @@
-package com.example.zelda;
 package test;
 
+// Importing the main MusicPlayer class to test it out.
+import main.MusicPlayer;
+// JUnit stuff for testing. Gotta make sure things actually work!
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+// Okay, now we're testing if the MusicPlayer actually works.
 public class MusicPlayerTest {
+
+    // This is where we test if the playSound method does its thing.
     @Test
     public void testPlaySound() {
+        // Creating a MusicPlayer object.
         MusicPlayer player = new MusicPlayer();
-        player.playSound("zelda-theme");
+        
+        // Playing a sound (well, just printing it for now).
+        player.playSound("zelda-theme"); // "Zelda-theme" is the test sound. Classic!
+        
+        // We're not really checking anything here, just running it to see if it prints correctly.
+        // Could improve this later, maybe mock the output?
     }
 }
