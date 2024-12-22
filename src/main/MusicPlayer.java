ce58d903fda53a5fd0e424c21f18f6e7fded91db
MichaelShpyl <150159875+MichaelShpyl@users.noreply.github.com>
2024-12-22 18:01:23 +0000

    Update MusicPlayer.java

--- src/main/MusicPlayer.java
+++ src/main/MusicPlayer.java
@@ -1,8 +1,10 @@
-package com.example.zelda;
 package main;
 
+// Alright, here's the main MusicPlayer class. It's super simple, just plays a sound.
 public class MusicPlayer {
+
+    // The only method here. It takes a sound and "plays" it (prints it to the console, lol).
     public void playSound(String sound) {
-        System.out.println("Playing sound: " + sound);
+        System.out.println("Playing sound: " + sound); // Literally just printing the sound.
     }
 }
